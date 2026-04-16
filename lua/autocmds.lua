172be9a3863a8a144787782b889dddbd23d1aeea
xonorhc <xonorhc@proton.me>
2026-04-16 16:58:14 -0400

    Update autocmds.lua

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("Signal", {
+  pattern = "SIGUSR1",
+  callback = function()
+    require("nvchad.utils").reload()
+  end,
+})
