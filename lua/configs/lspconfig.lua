976d94da18914943212fe152bf9afc399cd87db0
xonorhc <xonorhc@proton.me>
2026-04-16 16:58:55 -0400

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,19 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "bashls",
+  "dockerls",
+  "docker_compose_language_service",
+  "emmet_ls",
+  "hyprls",
+  "jsonls",
+  "lua_ls",
+  "marksman",
+  "pyright",
+  "yamlls",
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
