9a756f86c81b748ffa23bcf311541bfe9666fca9
xonorhc <xonorhc@proton.me>
2026-04-16 17:00:38 -0400

    Add plugins yazi.lua

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,28 @@
+return
+---@type LazySpec
+{
+  "mikavilpas/yazi.nvim",
+  version = "*", -- use the latest stable version
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+  init = function()
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
