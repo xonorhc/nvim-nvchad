ac296bc5abe5915dbb87c1bef3eceae0dfbd3dca
xonorhc <xonorhc@proton.me>
2026-04-16 16:57:20 -0400

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>ff", "<cmd> Telescope <cr>")
+map("t", "<ESC>", "<C-\\><C-n>")
+map("n", "<Leader>q", "<cmd> q <cr>")
+
+map("n", "<leader>tP", "<Cmd>w<CR> <Cmd>below split | term python %<CR> i", { noremap = true, silent = true })
+
+map("n", "<leader>uw", "<cmd>set wrap!<CR>", { desc = "Toggle line wrap" })
