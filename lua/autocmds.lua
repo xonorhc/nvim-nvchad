0f07ff45ada7a4d8f55299a387f356f277950b1c
xonorhc <xonorhc@proton.me>
2026-05-05 16:14:31 -0400

    Update autocmds.lua
    Add function to load dotenv
    Add function to set root dir

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -8,3 +8,29 @@ autocmd("Signal", {
     require("nvchad.utils").reload()
   end,
 })
+
+local function load_env()
+  local env_file = vim.fn.findfile(".env", ".;")
+  if env_file ~= "" then
+    for line in io.lines(env_file) do
+      if line:sub(1, 1) ~= "#" and line:find "=" then
+        local key, value = line:match "([^=]+)=(.*)"
+        if key and value then
+          value = value:gsub("^[\"']", ""):gsub("[\"']$", "")
+          vim.fn.setenv(key, value)
+        end
+      end
+    end
+  end
+end
+
+load_env()
+
+local function set_root()
+  local root = vim.fs.dirname(vim.fs.find({ ".git", ".env" }, { upward = true })[1])
+  if root then
+    vim.api.nvim_set_current_dir(root)
+  end
+end
+
+set_root()
