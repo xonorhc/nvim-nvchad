16f781e05681f876d416a5d772e8c026ef2d0728
xonorhc <xonorhc@proton.me>
2026-05-10 15:00:25 -0400

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,15 +4,23 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", { desc = "escape insert mode" })
 
-map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", ";", ":", { desc = "cmd enter command mode" })
+map("n", "<Leader>q", "<cmd>q<cr>", { desc = "window close" })
 
-map("n", "<leader>ff", "<cmd> Telescope <cr>")
-map("t", "<ESC>", "<C-\\><C-n>")
-map("n", "<Leader>q", "<cmd> q <cr>")
+map("n", "<leader>uw", "<cmd>set wrap!<CR>", { desc = "toggle line wrap" })
 
-map("n", "<leader>tP", "<Cmd>w<CR> <Cmd>below split | term python %<CR> i", { noremap = true, silent = true })
+map({ "n", "i", "v" }, "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
 
-map("n", "<leader>uw", "<cmd>set wrap!<CR>", { desc = "Toggle line wrap" })
+-- nvimtree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+
+-- terminal
+map("t", "<ESC>", "<C-\\><C-n>", { desc = "terminal escape terminal mode" })
+
+-- new terminals
+map("n", "<leader>tP", "<Cmd>w<CR><Cmd>below split | term python %<CR>i", { noremap = true, silent = true })
+
+-- telescope
+map("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", { desc = "search string under cursor" })
